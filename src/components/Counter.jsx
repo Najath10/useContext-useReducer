3502eb6aef8ac0354b98715826f59a13bc3529a0
Najath Mohammed <najathabdulla@gmail.com>
2024-09-01 01:30:21 +0530

    fixed error

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,11 +4,13 @@ import { CounterContext } from './Context'
 export const Counter = () => {
     const {increment,decrement,count} = useContext(CounterContext)
   return (
+
     <div>
-      {(count > 10 || count < 0) && (
+         <h1 >COUNT: {count}</h1>
+      {(count >= 10 || count <= 0) && (
          <h3 style={{ color: "red" }}>Limit Reached</h3>
             )}  
-    <h1 >COUNT: {count}</h1>
+   
     <button style={{marginRight:"10px", fontSize:"20px"}} onClick={increment}> + </button>
     <button  style={{marginRight:"10px", fontSize:"20px"}} onClick={decrement}> - </button>
 
