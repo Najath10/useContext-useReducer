14380114748dd0f0f51edb205ed1df90f61cb3b7
Najath Mohammed <najathabdulla@gmail.com>
2024-09-01 01:27:03 +0530

    added limit message

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,11 +5,13 @@ export const Counter = () => {
     const {increment,decrement,count} = useContext(CounterContext)
   return (
     <div>
-
+      {(count > 10 || count < 0) && (
+         <h3 style={{ color: "red" }}>Limit Reached</h3>
+            )}  
     <h1 >COUNT: {count}</h1>
     <button style={{marginRight:"10px", fontSize:"20px"}} onClick={increment}> + </button>
     <button  style={{marginRight:"10px", fontSize:"20px"}} onClick={decrement}> - </button>
 
-
+   
     </div>)
 }
